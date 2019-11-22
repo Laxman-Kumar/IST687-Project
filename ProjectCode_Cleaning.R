7d78989815ccf223874e6106ff8b4911513ef56b
bhavish2207 <bhavishkumar94@gmail.com>
2019-11-22 01:15:35 -0500

    Completed Data Cleaning. Bhavish

--- ProjectCode_Cleaning.R
+++ ProjectCode_Cleaning.R
@@ -18,66 +18,68 @@ View(fall)
 ####### - Cleaning Data Starts here  - ######
 ####### - BY - Laxman Kumar, Bhavish Kumar and Vidushi Mishra - #######
 
-    #Field with NAs DepartureDelay, ArrivalDelay, FlightTimeinMin
+    #Field with NAs are DepartureDelay, ArrivalDelay, FlightTimeinMin
     
-    ## DepartureDelay, ArrivalDelay, FlightTimesInMinutes(There are 33 rows with no combination of values)
+    ## All the NAs in Departure Delay column are due to flight cancellation.
+    ## There are 19 Nas in Arrival Delay and Flight Time in Minutes even for flights which are not cancelled and those have to be treated.
 
     columnNames <- c("DestinationCity","OriginCity","AirlineStatus","Age","Gender","PriceSensitivity",
                      "YearOfFirstFlight","FLightsPerYear","Loyalty","TypeOfTravel","TotalFreqFlyAccount",
                      "ShoppingAmount","FoodExpenses","Class","FlightDate","PartnerCode",
                      "ScheduleDepHour","DepartureDelayInMin","ArrivalDelayInMin",
-                     "FlightCancelledByUser","FlightDuration","Distance","LikelihoodRecommendScore",
-                     "OriginLong","OriginLat","DestLong","DestLat","DestinationState","OriginState")
+                     "FlightCancelled","FlightDuration","Distance","LikelihoodRecommendScore",
+                     "OriginLong","OriginLat","DestLong","DestLat","DestinationState","OriginState")     ## Creating new column names to be renamed
   
     #Hash mapping of partner code with the partner names
-    partnerCodeToName <- hash(unique(fall$Partner.Code),unique(fall$Partner.Name))
+    partnerCodeToName <- hash(unique(fall$Partner.Code),unique(fall$Partner.Name))     ## Creating a hash map between partner code and partner name
     
-    b <- data.frame(t(data.frame(strsplit(fall$Destination.City,","))))
+    b <- data.frame(t(data.frame(strsplit(fall$Destination.City,","))))             ## Removing the comma and state code from destination and origin city
     rownames(b) <- NULL
     a <- data.frame(t(data.frame(strsplit(fall$Origin.City,","))))
-    rownames(b) <- NULL
-      
-    fall$DestinationStateAbbr <- trimws(b$X2)
+    rownames(a) <- NULL
+    
+  
+    fall$DestinationStateAbbr <- trimws(b$X2)              ## Creating new Destination and origin state abbreviation columns
     fall$OriginStateAbbr <- trimws(a$X2)
     
-    StateMap <- hash(fall$OriginStateAbbr,fall$Origin.State)
+    StateMap <- hash(fall$OriginStateAbbr,fall$Origin.State)    ## Creating hash map between state abbreviation and state name
     
     df <- fall
     
-    df$Destination.City <- b$X1
+    df$Destination.City <- b$X1                   ## Reassigning cleaned values (after removing comma and state code) to dest & orig cities
     df$Origin.City <- a$X1
     
     remove(a)
     remove(b)
-    
+                                                 ## Removing the columns that are not necessary      
     df$Day.of.Month <- NULL
-    df$freeText <- NULL
+    df$freeText <- NULL              
     df$Origin.State <- NULL
     df$Destination.State <- NULL
     df$Partner.Name <- NULL
     
-    #Replacing NA in likelihood to mean of route of partner airline
+    #Replacing 1 NA in likelihood with mean at route & partner level
     averageLikelihood <- df %>%
       group_by(Origin.City, Destination.City, Partner.Code)%>%
       summarise(mean(Likelihood.to.recommend, na.rm=TRUE))
     
     indexOfNA <- which(is.na(df$Likelihood.to.recommend))
     df$Likelihood.to.recommend[indexOfNA] <-
-      as.integer(averageLikelihood[averageLikelihood$Origin.City==df[indexOfNA,2] & 
-                          averageLikelihood$Destination.City==df[indexOfNA,1] & 
+      as.integer(averageLikelihood[averageLikelihood$Origin.City==df[indexOfNA,"Origin.City"] & 
+                          averageLikelihood$Destination.City==df[indexOfNA,"Destination.City"] & 
                           averageLikelihood$Partner.Code==df[indexOfNA,"Partner.Code"],4])
     remove(indexOfNA)
     remove(averageLikelihood)
     
-    #Code for filtering out the Na when flight cancelled is Yes and No
+    #Code for filtering out the Na when flight cancelled is No
     
     df %>%
       select(Origin.City,Destination.City,Flight.cancelled,Flight.time.in.minutes) %>%
       filter(is.na(Flight.time.in.minutes) & Flight.cancelled=="No")
     
-    #Replacing NA in Flight time in minutes to mean of route of partner airline
-    #Assuming flight cancelled is customer data
-    averageFlightTime <- df %>%
+    #Replacing NA in Flight time in minutes when the flight was not cancelled, to mean at route & partner airline level
+    
+    averageFlightTime <- df %>%                                  ## Creating dataframe with average flight time in minutes at route and partner level
       group_by(Origin.City, Destination.City, Partner.Code)%>%
       summarise(mean(Flight.time.in.minutes, na.rm=TRUE))
     
@@ -86,14 +88,17 @@ View(fall)
     
     for(indexOfNA in indexOfNAList){
       df$Flight.time.in.minutes[indexOfNA] <-
-        as.integer(averageFlightTime[averageFlightTime$Origin.City==df[indexOfNA,2] & 
-                                       averageFlightTime$Destination.City==df[indexOfNA,1] & 
+        as.integer(averageFlightTime[averageFlightTime$Origin.City==df[indexOfNA,"Origin.City"] & 
+                                       averageFlightTime$Destination.City==df[indexOfNA,"Destination.City"] & 
                                        averageFlightTime$Partner.Code==df[indexOfNA,"Partner.Code"],4])
     }
-  
+    
+    ## Setting Flight Time in minutes to 0 for all cancelled flights.
     indexOfNAList <- which(is.na(df$Flight.time.in.minutes) & df$Flight.cancelled=="Yes")
     for(indexOfNA in indexOfNAList){df$Flight.time.in.minutes[indexOfNA] <- 0}
     
+    
+    ### For 3 route airline combinations, the average flight time in minutes was not obtained for which we manually found their flight time and assigned it
     indexOfNAList <- which(is.na(df$Flight.time.in.minutes) & df$Flight.cancelled=="No")
     df$Flight.time.in.minutes[indexOfNAList[1]] <- 90
     df$Flight.time.in.minutes[indexOfNAList[2]] <- 100
@@ -104,41 +109,36 @@ View(fall)
     remove(averageFlightTime)
     indexOfNAList <- which(is.na(df$Flight.time.in.minutes))
     
-    #Replacing Arrival and Departure delay
+    
+    #Replacing Arrival and Departure delay Nas. Only Arrival Delay column has 19 NAs wherever flight was not cancelled, 
+    ## which is treated in the same way as flight time in minutes
     
     averageArrivalDelay <- df %>%
       group_by(Origin.City, Destination.City, Partner.Code)%>%
       summarise(mean(Arrival.Delay.in.Minutes, na.rm=TRUE))
     
-    averageDepartureDelay <- df %>%
-      group_by(Origin.City, Destination.City, Partner.Code)%>%
-      summarise(mean(Departure.Delay.in.Minutes, na.rm=TRUE))
+    ## Departure Delaay has Nas only when flight was cancelled
     
-    indexArrivalDelay <- which(is.na(df$Arrival.Delay.in.Minutes))
-    indexDepartureDelay <- which(is.na(df$Departure.Delay.in.Minutes))
+    indexArrivalDelay <- which(is.na(df$Arrival.Delay.in.Minutes) & df$Flight.cancelled=="No")     ## Obtaining indexes where arrival delay is NA & flight was not cancelled
     
-    for(i in indexArrivalDelay){
+    ### Treating Nas for arrival delay column where the flight was not cancelled
+    for(i in indexArrivalDelay)
+    {
       df$Arrival.Delay.in.Minutes[i] <-
-        as.integer(averageArrivalDelay[averageArrivalDelay$Origin.City==df[i,2] & 
-                                         averageArrivalDelay$Destination.City==df[i,1] & 
+        as.integer(averageArrivalDelay[averageArrivalDelay$Origin.City==df[i,"Origin.City"] & 
+                                         averageArrivalDelay$Destination.City==df[i,"Destination.City"] & 
                                          averageArrivalDelay$Partner.Code==df[i,"Partner.Code"],4])
-      if(is.na(df$Arrival.Delay.in.Minutes[i])){
-        df$Arrival.Delay.in.Minutes[i] <-0
-      }
+      if(is.na(df$Arrival.Delay.in.Minutes[i]))
+        {
+        df$Arrival.Delay.in.Minutes[i] <-df$Departure.Delay.in.Minutes[i]
+        }
       
     } 
     
+    ### Replacing Nas where flight was cancelled with NaN (Not a number) for both Arrival Delay and Departure Delay columns.
     
-    for(i in indexDepartureDelay){
-      df$Departure.Delay.in.Minutes[i] <-
-        as.integer(averageDepartureDelay[averageDepartureDelay$Origin.City==df[i,2] & 
-                                           averageDepartureDelay$Destination.City==df[i,1] & 
-                                           averageDepartureDelay$Partner.Code==df[i,"Partner.Code"],4])
-      if(is.na(df$Departure.Delay.in.Minutes[i])){
-        df$Departure.Delay.in.Minutes[i] <-0
-      }
-      
-    }
+    df$Arrival.Delay.in.Minutes[which(is.na(df$Arrival.Delay.in.Minutes) & df$Flight.cancelled=="Yes")] <- NaN
+    df$Departure.Delay.in.Minutes[which(is.na(df$Departure.Delay.in.Minutes) & df$Flight.cancelled=="Yes")] <- NaN
     
     df2 <- df
     colnames(df) <- columnNames
@@ -156,8 +156,8 @@ View(fall)
     remove(df2)
     remove(indexOfNAList)
     saveRDS(df,file="CleanedData.Rda")
-    saveRDS(df,file="PartnerName.Rda")
-    saveRDS(df,file="StateName.Rda")
+    saveRDS(partnerCodeToName,file="PartnerName.Rda")
+    saveRDS(StateMap,file="StateName.Rda")
     
     
      
