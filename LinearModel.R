48cdf431e8e9f3ca225253780ef7ca1bd67b7da0
jnsabel <jnsabel@syr.edu>
2019-12-02 12:33:15 -0500

    Changed name of >54 in AgeGroup Column, Created 3 linear models that are in progress (waiting to see if I can fix FlightCancelled)

--- LinearModel.R
+++ LinearModel.R
@@ -1,13 +1,39 @@
-data <- load("CleanedData.Rda")
-View(df)
-df$AgeRange <- cut(df$Age, breaks=c(15,19,49,85), labels=c("Teenager","Adult","Elderly Adult"))
+data <- read.csv("cleandata.csv")
+data <- data.frame(data)
+View(data)
 
-#filter and remove HA
+###Making some changes to data
+data$TotalDelays <- data$DepartureDelayInMin+data$ArrivalDelayInMin
 
-regression1 <- lm(LikelihoodRecommendScore ~ . -DestinationCity -OriginCity -FlightDate -OriginState -DestinationState -DestLong -DestLat
-                  -OriginLong -OriginLat -Age, data=df)
+data$FlightCancelledNumeric <- as.numeric(data$FlightCancelled)
+#no is 1, yes is 2
+
+data$AirportSpending <- data$ShoppingAmount + data$FoodExpenses
+
+###Linear Models
+#First Test Regression - takes out Origin and Destination
+regression1 <- lm(LikelihoodRecommendScore ~ AirlineStatus + Age + AgeGroup + Gender + PriceSensitivity + Loyalty + TypeOfTravel
+                  + TotalFreqFlyAccount + AirportSpending + Class + ScheduleDepHour + TotalDelays + FlightCancelledNumeric + FlightDuration
+                  + Distance + PartnerCode, data=data)
+#error with singularities for FlightCancelledNumeric
 summary(regression1)
+#Adj R-Squared = .4224
 
-table(df$PartnerCode)
-table(df$TypeOfTravel)
-table(df$Class) 
+#Second Regression - Takes out Age (only age group) and FlightCancelledNumeric (since data didn't work)
+regression2 <- lm(LikelihoodRecommendScore ~ AirlineStatus + AgeGroup + Gender + PriceSensitivity + Loyalty + TypeOfTravel
+                  + TotalFreqFlyAccount + AirportSpending + Class + ScheduleDepHour + TotalDelays + FlightDuration
+                  + Distance + PartnerCode, data=data)
+summary(regression2)
+#Adj R-Squared = .4218
+
+
+#Third Regression - Takes out Age (only age group) and FlightCancelledNumeric (since data didn't work) and Loyalty and PriceSensitivity
+regression3 <- lm(LikelihoodRecommendScore ~ AirlineStatus + AgeGroup + Gender + TypeOfTravel
+                  + TotalFreqFlyAccount + AirportSpending + Class + ScheduleDepHour + TotalDelays + FlightDuration
+                  + Distance + PartnerCode, data=data)
+summary(regression3)
+#Adj R-Squared = .4213
+
+
+table(data$PartnerCode)
+
