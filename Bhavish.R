b8ec7294bf8c8fef07f1904bee643979ba840694
bhavish2207 <bhavishkumar94@gmail.com>
2019-12-03 19:36:06 -0500

    Linear Models 4: High performing cancelled & not cancelled, Low Performing cancelled & not cancelled

--- Bhavish.R
+++ Bhavish.R
@@ -40,68 +40,102 @@ partner_nps$nps <- ((partner_nps$number_promoters - partner_nps$number_detractor
 
 
 library(ggplot2)
-partner_nps_plot <- ggplot(data = partner_nps, aes(x = partner_nps$PartnerCode, y = partner_nps$nps))    ## Plotting NPS score for each partner
+partner_nps_plot <- ggplot(data = partner_nps, aes(x = reorder(partner_nps$PartnerCode,partner_nps$nps), y = partner_nps$nps, fill = partner_nps$nps, xlab("Partners"), ylab("NPS")))    ## Plotting NPS score for each partner
 partner_nps_plot <- partner_nps_plot + geom_col()
 partner_nps_plot
 
 ## CREATING DF by removing partner HA that has very less observations and other partners with high NPS scores
-    ## HA has >50%, AS, OO & DL have around 19%. Hence these 4 partners are removed from our linear model
+    ## HA has >50%, AS, OO & DL have around 19%. Hence these 4 partners are removed from our linear model. Also VX is removed as it has very few rows.
 
 lm_df_partner_rem <- lm_df[(lm_df$PartnerCode != 'HA') & (lm_df$PartnerCode != 'AS') & (lm_df$PartnerCode !='OO') & (lm_df$PartnerCode !='DL'),]
-unique(lm_df$FlightCancelled)
 
-##Linear Model for non cancelled flights  ADJ R square = 49.33 %
 
-linearmodel_non_cancel <- lm(LikelihoodRecommendScore ~ AirlineStatus + Age + Gender + PriceSensitivity + Loyalty + TypeOfTravel + TotalFreqFlyAccount +
-                       FoodExpenses + FLightsPerYear +ShoppingAmount + Class + ScheduleDepHour+ total_delay + PartnerCode + FlightDuration + Distance + DestinationState + OriginState,
+##Linear Model for non cancelled flights after excluding high performing partners and partners with low number of observations. ADJ R square = 49.3 %
+
+linearmodel_non_cancel <- lm(LikelihoodRecommendScore ~ AirlineStatus + Age + Gender + PriceSensitivity + Loyalty + TypeOfTravel + 
+                       FoodExpenses + FLightsPerYear +ShoppingAmount + Class + ScheduleDepHour+ total_delay + PartnerCode + FlightDuration + Distance + OriginState + DestinationState,
                      data = lm_df_partner_rem[lm_df_partner_rem$FlightCancelled == 'No',])
 summary(linearmodel_non_cancel)      
 
 
-## Generate Training and Test Data for the abve built model to measure accuracy.
+## Linear model for cancelled flights after excluding high performing partners and partners with low number of observations. ADJ R square = 38.07 %
 
-randindex <- sample(1:dim(lm_df_partner_rem))
-cut_point2_3 <- floor(3*dim(lm_df_partner_rem)[1]/4)
-train_non_cancel <- lm_df_partner_rem[randindex[1:cut_point2_3],]    ### 66.6% of data is used as Training Data
-
-test_non_cancel <- lm_df_partner_rem[randindex[(cut_point2_3+1):dim(lm_df_partner_rem)[1]],]
-
-linearmodel_train_non_cancel <- lm(LikelihoodRecommendScore ~ AirlineStatus + Age + Gender + PriceSensitivity + Loyalty + TypeOfTravel +
-                              FoodExpenses + FLightsPerYear + ShoppingAmount + Class + total_delay + PartnerCode + FlightDuration + DestinationState + OriginState,
-                             data = train_non_cancel)
-summary(linearmodel_train_non_cancel)
-
-test_predictions <-predict(linearmodel_train_non_cancel,test_non_cancel)
-comparison_table <- data.frame(test_non_cancel$LikelihoodRecommendScore, round(test_predictions,0))
-colnames(comparison_table) <- c('actual','predicted')
-length(which(comparison_table$actual==comparison_table$predicted))/length(comparison_table$actual)
-
-
-## Linear model for cancelled flights has ADJ R square = 48.5 %
-
-linearmodel_cancel <- lm(LikelihoodRecommendScore ~ AirlineStatus + Age + Gender + PriceSensitivity + Loyalty + TypeOfTravel + TotalFreqFlyAccount +
+linearmodel_cancel <- lm(LikelihoodRecommendScore ~ AirlineStatus + Age + Gender + PriceSensitivity + Loyalty + TypeOfTravel + 
                        FoodExpenses + FLightsPerYear +ShoppingAmount + Class + ScheduleDepHour+ PartnerCode + FlightDuration + Distance + DestinationState + OriginState,
                      data = lm_df_partner_rem[lm_df_partner_rem$FlightCancelled == 'Yes',])
 summary(linearmodel_cancel)
 
+
+
+## Including High performing partners and building a model for the df
+
+lm_df_partner_incl <- lm_df[(lm_df$PartnerCode == 'HA') | (lm_df$PartnerCode == 'AS') | (lm_df$PartnerCode == 'OO') | (lm_df$PartnerCode == 'DL'),]
+
+
+##Linear Model for non cancelled flights after including high performing partners and partners with low number of observations. ADJ R square = 39.43 %
+
+linearmodel_non_cancel_highpf <- lm(LikelihoodRecommendScore ~ AirlineStatus + Age + Gender + PriceSensitivity + Loyalty + TypeOfTravel + 
+                               FoodExpenses + FLightsPerYear +ShoppingAmount + Class + ScheduleDepHour+ total_delay + PartnerCode + FlightDuration + Distance + OriginState + DestinationState,
+                             data = lm_df_partner_incl[lm_df_partner_incl$FlightCancelled == 'No',])
+summary(linearmodel_non_cancel_highpf)      
+
+
+## Linear model for cancelled flights after including high performing partners and partners with low number of observations. ADJ R square = 86.43 %
+
+linearmodel_cancel_highpf <- lm(LikelihoodRecommendScore ~ AirlineStatus + Age + Gender + PriceSensitivity + Loyalty + TypeOfTravel + 
+                           FoodExpenses + FLightsPerYear +ShoppingAmount + Class + ScheduleDepHour+ PartnerCode + FlightDuration + Distance + DestinationState + OriginState,
+                         data = lm_df_partner_incl[lm_df_partner_incl$FlightCancelled == 'Yes',])
+summary(linearmodel_cancel_highpf)
+
+    ### *** END OF LINEAR MODEL CODE *** ###
+  
+## APPENDIX/POTENTIAL ENHANCEMENTS
+
+## Generate Training and Test Data for the abve built model to measure accuracy.
+## NOT POSSIBLE TO BUILD STRATIFIED SAMPLE OF TRAINING & TEST DATASET because Origin & Destination state columns have been included.
+
+#lm_df_partner_rem_cancel <- lm_df_partner_rem[lm_df_partner_rem$FlightCancelled == 'Yes',]
+#View(lm_df_partner_rem_cancel)
+
+#randindex1 <- sample(1:dim(lm_df_partner_rem_cancel))
+#cut_point2_3 <- floor(2*dim(lm_df_partner_rem_cancel)[1]/3)
+#train_cancel <- lm_df_partner_rem[randindex1[1:cut_point2_3],]    ### 66.6% of data is used as Training Data
+
+#test_cancel <- lm_df_partner_rem[randindex[(cut_point2_3+1):dim(lm_df_partner_rem)[1]],]
+
+#linearmodel_train_cancel <- lm(LikelihoodRecommendScore ~ AirlineStatus + Age + Gender + PriceSensitivity + Loyalty + TypeOfTravel +
+ #                                    FoodExpenses + FLightsPerYear + ShoppingAmount + Class + total_delay + PartnerCode + FlightDuration + DestinationState + OriginState,
+  #                                 data = train_cancel)
+#summary(linearmodel_train_cancel)
+
+#test_predictions <-predict(linearmodel_train_cancel,test_cancel)
+#comparison_table <- data.frame(test_non_cancel$LikelihoodRecommendScore, round(test_predictions,0))
+#colnames(comparison_table) <- c('actual','predicted')
+#length(which(comparison_table$actual==comparison_table$predicted))/length(comparison_table$actual)
+
 ## Generate Training and Test Data for the abve built model to measure accuracy.
 
-lm_df_partner_rem_cancel <- lm_df_partner_rem[lm_df_partner_rem$FlightCancelled == 'Yes',]
-View(lm_df_partner_rem_cancel)
+##library(tidyverse)
+#no_orig_dest_states <- lm_df_partner_rem %>%
+# group_by(OriginState,DestinationState) %>% 
+#count(recommender_type)
+#View(no_orig_dest_states)
+#colnames(no_orig_dest_states) <- c('OriginState','DestinationState','recommender_type','number_passengers')
 
-randindex1 <- sample(1:dim(lm_df_partner_rem_cancel))
-cut_point2_3 <- floor(2*dim(lm_df_partner_rem_cancel)[1]/3)
-train_cancel <- lm_df_partner_rem[randindex1[1:cut_point2_3],]    ### 66.6% of data is used as Training Data
 
-test_cancel <- lm_df_partner_rem[randindex[(cut_point2_3+1):dim(lm_df_partner_rem)[1]],]
+#randindex <- sample(1:dim(lm_df_partner_rem))
+#cut_point2_3 <- floor(2*dim(lm_df_partner_rem)[1]/3)
+#train_non_cancel <- lm_df_partner_rem[randindex[1:cut_point2_3],]    ### 66.6% of data is used as Training Data
 
-linearmodel_train_cancel <- lm(LikelihoodRecommendScore ~ AirlineStatus + Age + Gender + PriceSensitivity + Loyalty + TypeOfTravel +
-                                     FoodExpenses + FLightsPerYear + ShoppingAmount + Class + total_delay + PartnerCode + FlightDuration + DestinationState + OriginState,
-                                   data = train_cancel)
-summary(linearmodel_train_cancel)
+#test_non_cancel <- lm_df_partner_rem[randindex[(cut_point2_3+1):dim(lm_df_partner_rem)[1]],]
 
-test_predictions <-predict(linearmodel_train_cancel,test_cancel)
-comparison_table <- data.frame(test_non_cancel$LikelihoodRecommendScore, round(test_predictions,0))
-colnames(comparison_table) <- c('actual','predicted')
-length(which(comparison_table$actual==comparison_table$predicted))/length(comparison_table$actual)
+#linearmodel_train_non_cancel <- lm(LikelihoodRecommendScore ~ AirlineStatus + Age + Gender + PriceSensitivity + Loyalty + TypeOfTravel +
+ #                                    FoodExpenses + FLightsPerYear + ShoppingAmount + Class + total_delay + PartnerCode + FlightDuration,
+  #                                 data = train_non_cancel)
+#summary(linearmodel_train_non_cancel)
+
+#test_predictions <-predict(linearmodel_train_non_cancel,test_non_cancel)
+#comparison_table <- data.frame(test_non_cancel$LikelihoodRecommendScore, round(test_predictions,0))
+#colnames(comparison_table) <- c('actual','predicted')
+#length(which(comparison_table$actual==comparison_table$predicted))/length(comparison_table$actual)
 
